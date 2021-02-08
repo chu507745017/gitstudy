c92c6714a28511277069f8ad217510a2afbcf80d
chujiahui <5308533+chujiahui@user.noreply.gitee.com>
2021-02-08 15:57:39 +0800

    '第二次提交'

---
+++ src/com/chu/A.java
@@ -0,0 +1,11 @@
+package com.chu;
+
+/**
+ * @author jhc
+ * @date 2021/2/8 15:48
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("ajfsdf");
+    }
+}
---
+++ src/com/chu/init.java
@@ -0,0 +1,8 @@
+package com.chu;
+
+/**
+ * @author jhc
+ * @date 2021/2/8 15:48
+ */
+public class init {
+}
